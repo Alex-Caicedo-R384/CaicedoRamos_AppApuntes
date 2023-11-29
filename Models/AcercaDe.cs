61d687bebadaecbf089bc00684336f50c09e7194
Alex Caicedo Ramos <alex.caicedo@udla.edu.ec>
2023-11-29 09:17:38 -0500

    5to Commit: Mejora del diseño para una apariencia más atractiva en la página de Todas las Notas."

--- Models/AcercaDe.cs
+++ Models/AcercaDe.cs
@@ -9,8 +9,6 @@ namespace CaicedoRamos_AppApuntes.Models
     internal class AcercaDe
     {
         public string Title => AppInfo.Name;
-        public string Version => AppInfo.VersionString;
         public string MoreInfoUrl => "https://aka.ms/maui";
-        public string Message => "Esta aplicacion esta hecha en XAML y C# con .NET MAUI.";
     }
 }
